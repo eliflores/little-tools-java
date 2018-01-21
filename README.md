09566ad0e83e4d7539bda60e197dbc69c5dd5135
mignonnesaurus <elifloresch@gmail.com>
2018-01-21 19:34:01 +0100

    Implement JsonWriter! :)

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Little tools that I find myself needing from time to time
 
 1) HttpClient - ✓
 2) URL - encode / decode - ✓
-3) JSON - Read ✓ / Write 
+3) JSON - Read ✓ / Write - ✓
 4) CSV - Read / Write 
 5) XML - Read / Write
 
--- src/main/java/com/mignonnesaurus/tools/json/JsonWriter.java
+++ src/main/java/com/mignonnesaurus/tools/json/JsonWriter.java
@@ -1,4 +1,26 @@
 package com.mignonnesaurus.tools.json;
 
-public class JsonWriter {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mignonnesaurus.tools.exception.ExceptionUtil;
+
+import java.io.IOException;
+
+public final class JsonWriter {
+    private static final ObjectMapper OBJECT_MAPPER;
+
+    static {
+        OBJECT_MAPPER = new ObjectMapper();
+    }
+
+    public String toJson(Object object) {
+        try {
+            return getObjectMapper().writeValueAsString(object);
+        } catch (IOException e) {
+            throw ExceptionUtil.createException("Error while reading JSON", e);
+        }
+    }
+
+    private static ObjectMapper getObjectMapper() {
+        return OBJECT_MAPPER;
+    }
 }
