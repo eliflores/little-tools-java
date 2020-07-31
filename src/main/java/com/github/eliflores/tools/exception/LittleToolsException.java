f07a175d79587d8fcb1824c93cda36842ee8a318
mignonnesaurus <eli@mignonnesaurus.com>
2020-08-01 00:09:41 +0200

    Rename MyLittleToolsException to LittleToolsException

---
+++ src/main/java/com/github/eliflores/tools/exception/LittleToolsException.java
@@ -0,0 +1,7 @@
+package com.github.eliflores.tools.exception;
+
+public class LittleToolsException extends RuntimeException {
+    public LittleToolsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
